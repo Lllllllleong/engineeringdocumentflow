e1107d833f1d79369f79bbcc040c42e36ba276d8
Leong Yin Chan <u6858120@anu.edu.au>
2025-07-25 15:51:47 +0800

    Single func added

--- cmd/page-translator/main.go
+++ cmd/page-translator/main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/Lllllllleong/engineeringdocumentflow/internal/models"
 	"github.com/Lllllllleong/engineeringdocumentflow/internal/services"
-		_ "github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	_ "github.com/GoogleCloudPlatform/functions-framework-go/functions"
 )
 
 var (
